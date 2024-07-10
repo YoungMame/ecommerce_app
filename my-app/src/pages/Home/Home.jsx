20b059d6cad348b8096378236cfb8d7df61749b8
Young <mame.duvey@gmail.com>
2024-07-10 15:45:53 +0200

    Creation du scss de l'app

--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -3,7 +3,13 @@ import "./home.scss"
 export default function Home() {
     return (
         <>
-        Voici le main menu
+            <h1>Titre1</h1>
+            <h2>Titre2</h2>
+            <h3>Titre3</h3>
+            <h4>Titre4</h4>
+            <h5>Titre5</h5>
+            <p>Texte</p>    
+            <button className="bg-primary">Cliquez</button> 
         </>
     )
 }
